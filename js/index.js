cc9c4c37353cd4f0fb736eb00e0f9e576b427434
Astrospence <illustrator.wood@gmail.com>
2021-08-31 14:16:33 -0600

    1 event listener finished

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const nav = document.querySelectorAll('header > div > nav > a');
+nav.forEach(cb => {
+    cb.addEventListener('mouseover', event => {
+        cb.style.opacity = '0.5';
+    })
+})
