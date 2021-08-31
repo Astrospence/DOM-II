a07710f0bc97cc81d542041c24b44db419920a9b
Astrospence <illustrator.wood@gmail.com>
2021-08-31 14:50:45 -0600

    3 event listeners finished

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
 // Your code goes here
+//#1
 const nav = document.querySelectorAll('header > div > nav > a');
 nav.forEach(cb => {
     cb.addEventListener('mouseover', event => {
         cb.style.opacity = '0.5';
     })
 })
+//#2
+nav.forEach(cb => {
+    cb.addEventListener('mouseout', event => {
+        cb.style.opacity = '100';
+    })
+})
+//#3
+const body = document.querySelector('body');
+body.addEventListener('keydown', event => {
+    if (body.style.backgroundColor === 'lightblue'){
+        body.style.backgroundColor = 'white';
+    }else{
+    body.style.backgroundColor = 'lightblue';
+    }
+}) 
+//#4
